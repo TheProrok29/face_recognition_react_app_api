21822d08ec7eaeb26f4370929ca3961d93a06754
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-03 20:20:15 +0100

    Add testing  /signin endpoint

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const expresss = require('express');
+const bodyParser = require('body-parser');
 
 const app = expresss();
 
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Tom',
+            email: 'tom@gmail.com',
+            password: 'password',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '1234',
+            name: 'Ida',
+            email: 'ida@gmail.com',
+            password: 'password2',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 app.get('/', (req, res) => {
     res.send('this is working')
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
 });
 
 app.listen(3000, () => {
