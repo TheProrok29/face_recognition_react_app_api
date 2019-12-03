a788424302f7cca9c7c2bc3a3c4d47a0d0d79a66
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-03 21:08:42 +0100

    Add testing /profile:id endpoint to get a user

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     database.users.push({
-        id: '123',
+        id: '12345',
         name: name,
         email: email,
         password: password,
@@ -49,6 +49,20 @@ app.post('/register', (req, res) => {
         joined: new Date()
     });
     res.json(database.users[database.users.length - 1])
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+    if (!found) {
+        res.status(400).json('not found');
+    }
 });
 
 app.listen(3000, () => {
